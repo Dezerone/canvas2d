242601cea9ada752810580e0aefc26a80881fa2e
Dezerone <zamindenis1@gmail.com>
2023-11-21 20:29:34 +0400

    21

--- js/graph.js
+++ js/graph.js
@@ -1,10 +1,10 @@
 function Graph(options) {
   options = options || {};
-  var id = options.id;
-  var width = options.width || 300;
-  var height = options.height || 300;
-  var WIN = options.WIN || {};
-  var canvas;
+  const id = options.id;
+  const width = options.width || 300;
+  const height = options.height || 300;
+  const WIN = options.WIN || {};
+  let canvas;
   if (id) {
     canvas = document.getElementById(id);
   } else {
@@ -13,53 +13,80 @@ function Graph(options) {
   }
   canvas.width = width;
   canvas.height = height;
-  var context = canvas.getContext('2d');
-  var callbacks = options.callbacks;
+
+  const ctx = canvas.getContext('2d');
+
+  const PI2 = Math.PI * 2;
+  const callbacks = options.callbacks;
   canvas.addEventListener('wheel', callbacks.wheel);
-  canvas.addEventListener('mousedown', callbacks.moveWindow);
-  canvas.addEventListener('mousemove', callbacks.moveWindow);
-  canvas.addEventListener('mouseup', callbacks.moveWindow);
-  
+  canvas.addEventListener('mousemove', callbacks.mousemove);
+  canvas.addEventListener('mousedown', callbacks.mousedown);
+  canvas.addEventListener('mouseup', callbacks.mouseup);
+  canvas.addEventListener('mouseout', callbacks.mouseout);
 
-  const PI2 = 2 * Math.PI;
-
-  function xs(x) {
-    return (x - WIN.left) * canvas.width / WIN.width;
+  const xs = (x) => {
+    return (canvas.width * (x - WIN.left) / WIN.width);
   }
-  function ys(y) {
-    return canvas.height - ((y - WIN.bottom) * canvas.height / WIN.height);
+
+  const ys = (y) => {
+    return (canvas.height - (canvas.height * (y - WIN.bottom) / WIN.height));
   }
 
   this.clear = function () {
-    context.fillStyle = '#efe';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 
   this.line = function (x1, y1, x2, y2, color, width, isDash) {
-    context.beginPath();
-    context.strokeStyle =color ||'orange';
-    if(isDash){ 
-      context.setLineDash([1, 10]);
-    }else{
-      context.setLineDash([]);
+    ctx.beginPath();
+    isDash = isDash || false;
+    ctx.strokeStyle = color || 'black';
+    ctx.lineWidth = width || 3;
+    if (isDash) {
+      ctx.setLineDash([10, 10]);
     }
-    context.lineWidth = width || 1;
-    context.moveTo(xs(x1), ys(y1));
-    context.lineTo(xs(x2), ys(y2));
-    context.stroke();
-    context.closePath();
+    else {
+      ctx.setLineDash([0, 0]);
+    }
+    ctx.moveTo(xs(x1), ys(y1));
+    ctx.lineTo(xs(x2), ys(y2));
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  this.text = function (text, x, y, color, size, font) {
+    ctx.beginPath();
+    font = font || 'Arial';
+    size = size || "18";
+    ctx.font = size + "px " + font;
+    ctx.fillStyle = color || 'black';
+    ctx.fillText(text, xs(x), ys(y));
+    ctx.closePath();
   }
 
   this.point = function (x, y, color, size) {
-    context.beginPath();
-    context.strokeStyle = color || '#f0f';
-    context.arc(xs(x), ys(y), size || 2, 0, PI2);
-    context.stroke();
-    context.closePath();
+    ctx.beginPath();
+    ctx.strokeStyle = color || 'white';
+    ctx.arc(xs(x), ys(y), size || 1, 0, PI2);
+    ctx.stroke();
+    ctx.closePath();
   } 
-  this.text=function(text,x,y,color){
-    context.fullStyle=color||'#f0f';
-    context.font="20px Arial";
-    context.fillText(text,xs(x),ys(y));
-  }
+  var positionsButton= 0; 
+  const green = '#1be81b';
+  const red = '#e81b1b';
+  const moveButton = document.getElementById("choiseMove");
+  const lineButton = document.getElementById("choiseLine");
+
+  // Добавляем обработчик события "click" для каждой кнопки
+  moveButton.addEventListener("click", function () {
+    console.log("Нажата кнопка 'choiseMove'"); 
+    positionsButton=1;
+    console.log(positionsButton); 
+  });
+
+  lineButton.addEventListener("click", function () {
+    console.log("Нажата кнопка 'choiseLine'");  
+    positionsButton=0;
+    console.log(positionsButton); 
+  });
 }
--- js/main.js
+++ js/main.js
@@ -17,18 +17,45 @@ window.onload = function () {
         left: -10,
         bottom: -10,
     };
-    var isDragging = false;
-    var startX, startY;
-
     var zoomStep = 2;
 
-    var graph = new Graph({
+    const mousedown = () => {
+            canMove = true;
+    }
+
+    const mouseup = () => {
+        canMove = false;
+    }
+
+    const mouseout = () => {
+        canMove = false;
+    }
+    const mousemove = (event) => {
+        console.log(Graph.positionsButton); 
+        if ((canMove)&&(Graph.positionsButton)) {
+            WIN.left -= event.movementX / (canvas.width / WIN.width);
+            WIN.bottom += event.movementY / (canvas.height / WIN.height);
+            render();
+        }
+    }
+
+    const graph = new Graph({
         id: 'canvas',
-        width: 600,
-        height: 600,
-        WIN: WIN,
-        callbacks: { wheel: wheel, moveWindow: moveWindow }
-    });
+        width: 500,
+        height: 500,
+        WIN: WIN, 
+        callbacks: {
+            wheel,
+            mousemove,
+            mousedown,
+            mouseup,
+            mouseout
+        }
+    }
+    )
+
+
+    let canMove = false;
 
 
     function wheel(event) {
@@ -39,30 +66,13 @@ window.onload = function () {
         WIN.bottom -= delta / 2;
         render();
     }
-    function moveWindow(event) {
-        if (event.type === "mousedown") {
-            isDragging = true;
-            startX = event.clientX;
-            startY = event.clientY;
-        } else if (event.type === "mousemove" && isDragging) {
-            var offsetX = event.clientX - startX;
-            var offsetY = event.clientY - startY;
 
-            WIN.left -= offsetX;
-            WIN.bottom += offsetY;
-            startX = event.clientX;
-            startY = event.clientY;
-            render();
-        } else if (event.type === "mouseup") {
-            isDragging = false;
-        }
-    }
     function printFunction(f, n, color) {
         var x = WIN.left;
         var dx = WIN.width / n;
         while (x <= WIN.width + WIN.left) {
             var isDash = Math.abs(f(x) - f(x - dx)) >= WIN.height;
-            graph.line(x, f(x), x + dx, f(x + dx), color, 2, isDash);
+            graph.line(x, f(x), x + dx, f(x + dx), color, 4, isDash);
 
             if ((Math.sign(f(x)) != Math.sign(f(x + dx))) && !isDash) {
                 printZero(x);
@@ -102,6 +112,18 @@ window.onload = function () {
         for (var i = Math.floor(WIN.bottom); i > WIN.bottom; i++) {
             graph.line(WIN.left, i, WIN.left + WIN.width, i, '#0003');
         }
+        graph.line(WIN.width + WIN.left, 0, WIN.width + WIN.left - WIN.width / 100, WIN.height / 100);
+        graph.line(WIN.width + WIN.left - WIN.width / 100, WIN.height / 100, WIN.width + WIN.left - WIN.width / 100, -WIN.height / 100);
+        graph.line(WIN.width + WIN.left - WIN.width / 100, -WIN.height / 100, WIN.width + WIN.left, 0);
+        //рисование стрелки y
+        graph.line(0, WIN.height + WIN.bottom, WIN.width / 100, WIN.height + WIN.bottom - WIN.height / 100);
+        graph.line(WIN.width / 100, WIN.height + WIN.bottom - WIN.height / 100, -WIN.width / 100, WIN.height + WIN.bottom - WIN.height / 100);
+        graph.line(-WIN.width / 100, WIN.height + WIN.bottom - WIN.height / 100, 0, WIN.height + WIN.bottom);
+
+        //подпись осей
+        graph.text('X', WIN.width + WIN.left - WIN.width / 50, -WIN.height / 25);
+        graph.text('Y', WIN.width / 34, WIN.height + WIN.bottom - WIN.height / 34);
+        graph.text('O', WIN.width / 100, WIN.height / 100);
     }
     function printZero(xZeros) {
         graph.point(xZeros, 0);
@@ -109,7 +131,7 @@ window.onload = function () {
     function render() {
         graph.clear();
         printOXY();
-        printFunction(f, 19994, 'black');
+        printFunction(f, 155, 'blue');
         printZero(f)
     }
 
